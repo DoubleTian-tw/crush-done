60b8e54ee7ca7be29301a0a036a99f3a8c77bd28
Paddy Tian <q135135x@gmail.com>
2026-02-15 13:52:52 +0800

    update: Update postgres connect string

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.POSTGRES_CONNECTION_STRING,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 })
 
